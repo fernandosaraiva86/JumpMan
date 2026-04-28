579f49495e6f75da4080838078a6c338e95c9e82
fernandosaraiva86 <fernandosaraiva86@gmail.com>
2026-04-28 19:10:01 -0300

    JumpMan

---
+++ Assets/Script/MoveLeft.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+
+
+    [SerializeField] private float speed = 10f;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,56 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+
+    public float gravityModifier = 1f;
+    public float jumpForce = 10f;
+    public bool isOnGround = true;
+
+    //animação
+    private Animator playerAnim;
+
+    //Game Over
+    public bool gameOver = false;
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+    }
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+            isOnGround = false;
+
+            //animação
+            playerAnim.SetTrigger("Jump_trig");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        playerRb.AddForce(Vector3.down * 
+            (gravityModifier -1) * Physics.gravity.magnitude, ForceMode.Acceleration);
+    }
+}
